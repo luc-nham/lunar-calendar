dd7bb077f74504f1da91943650d35c7f0c351134
vantran445 <CaoVan.Info@gmail.com>
2023-04-24 22:42:44 +0700

    Add test lunar new moons

--- tests/Lunar/VN/LunarDateTimeCorrectorTest.php
+++ tests/Lunar/VN/LunarDateTimeCorrectorTest.php
@@ -29,6 +29,17 @@ class LunarDateTimeCorrectorTest extends BaseTest
         $this->assertEquals($midnightJd, $nm->getMidnightJd());
     }
 
+    /**
+     * Kiểm tra tháng nhuận Âm lịch
+     * 
+     * @param mixed $lunarYear 
+     * @param mixed $leapMonth 
+     * @param mixed $newMoonDate 
+     * @param mixed $timestamp 
+     * @param mixed $midnightJd 
+     * @return void 
+     * @throws ExpectationFailedException 
+     */
     #[DataProviderExternal(LunarLeapMonthProvider::class, 'listOf20thCentury')]
     #[DataProviderExternal(LunarLeapMonthProvider::class, 'listOf21thCentury')]
     public function testLeapMonth($lunarYear, $leapMonth, $newMoonDate, $timestamp, $midnightJd): void
@@ -45,5 +56,27 @@ class LunarDateTimeCorrectorTest extends BaseTest
             $lunarYear
         ));
         $this->assertEquals($midnightJd, $leap->getMidnightJd());
+    }
+
+    /**
+     * Kiểm tra điểm Sóc của các tháng Âm lịch trong năm
+     * 
+     * @param string $year 
+     * @param string $month 
+     * @param string $newMoonDate 
+     * @param int $timestamp 
+     * @param float $midnightJd 
+     * @return void 
+     * @throws ExpectationFailedException 
+     */
+    #[DataProviderExternal(LunarNewMoonProvider::class, 'listOf20thCentury')]
+    #[DataProviderExternal(LunarNewMoonProvider::class, 'listOf21thCentury')]
+    public function testNewMoon(string $year, string $month, string $newMoonDate, int $timestamp, float $midnightJd): void
+    {
+        $parser = new LunarParser("15/$month/$year", self::getTimeZone());
+        $corrector = new LunarDateTimeCorrector($parser);
+        $newMoon = $corrector->getNewMoon();
+
+        $this->assertEquals($midnightJd, $newMoon->getMidnightJd());
     }
 }
