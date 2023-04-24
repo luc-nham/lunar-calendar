3066e513bd7b8baadcf3bc4b8946339048b2d525
vantran445 <CaoVan.Info@gmail.com>
2023-04-24 21:22:50 +0700

    Fix code

--- src/Lunar/LunarLeapMonth.php
+++ src/Lunar/LunarLeapMonth.php
@@ -2,130 +2,114 @@
 
 use Exception;
 use VanTran\LunarCalendar\Mjd\BaseMjd;
+use VanTran\LunarCalendar\MoonPhases\Lunar11thNewMoonPhase;
 use VanTran\LunarCalendar\MoonPhases\Lunar11thNewMoonPhaseInterface;
 use VanTran\LunarCalendar\MoonPhases\NewMoonPhaseInterface;
 use VanTran\LunarCalendar\Sunlongitude\MjdToSunlongitude;
 
+/**
+ * Lớp tính toán tháng nhuận âm lịch từ điểm Sóc tháng 11 của năm Âm lịch
+ * 
+ * @author Văn Trần <caovan.info@gmail.com>
+ * @package VanTran\LunarCalendar\Lunar
+ */
 class LunarLeapMonth extends BaseMjd implements LunarLeapMonthInterface
 {
     /**
-     * @var null|int Vị trí (số) tháng nhuận Âm lịch nếu có
+     * @var null|int Số (vị trí) tháng nhuận âm lịch, null nếu không có tháng nhuận
      */
     protected $month;
 
     /**
      * Tạo đối tượng mới
      * 
-     * @param Lunar11thNewMoonPhaseInterface $newMoon11th Điểm sóc tháng 11 của năm Âm lịch
+     * @param Lunar11thNewMoonPhaseInterface $newMoon11th Điểm Sóc tháng 11 của 1 năm Âm lịch
      * @return void 
      * @throws Exception 
      */
     public function __construct(protected Lunar11thNewMoonPhaseInterface $newMoon11th)
     {
-        $this->offset = $newMoon11th->getOffset();
-        $this->init();
-    }
-
-    /**
-     * Xác định 1 tháng có thể trở thành tháng nhuận hay không dựa vào điểm Sóc của tháng đó và điểm Sóc của tháng kế
-     * tiếp. 1 tháng có thể trở thành tháng nhuận khi nó không chứa điểm Trung khí nào.
-     * 
-     * @param NewMoonPhaseInterface $newMoon 
-     * @param NewMoonPhaseInterface $nextNewMoon 
-     * @return bool 
-     */
-    public function canMonthBeLeap(NewMoonPhaseInterface $newMoon, NewMoonPhaseInterface $nextNewMoon): bool
-    {
-        $leap = false;
-        $sl = new MjdToSunlongitude($newMoon);
-        $nexSl = new MjdToSunlongitude($nextNewMoon);
-
-        if (floor($sl->getMidnightDegrees() / 30) == floor($nexSl->getMidnightDegrees() / 30)) {
-            $leap = true;
+        // Nếu năm đó không nhuận thì không cần tính nữa
+        if (in_array($this->newMoon11th->getYear() % 19, [0, 3, 6, 9, 11, 14, 17])) {
+            $this->init();
         }
-
-        return $leap;
     }
 
     /**
-     * Thu thập các tháng có thể trở thành tháng nhuận trong năm
-     * @return array 
-     */
-    protected function collectMonthsCanBeLeap(): array
-    {
-        $month = 12;
-        $months = [];
-
-        $nextNewMoon = $this->newMoon11th->add(2);
-
-        for ($i = 0; $i < 13; $i ++) {
-            $newMoon = $nextNewMoon->subtract(1);
-
-            if ($this->canMonthBeLeap($newMoon, $nextNewMoon)) {
-                $months[] = [
-                    'month' => $month,
-                    'new_moon' => $newMoon
-                ];
-            }
-
-            $nextNewMoon = $newMoon;
-            $month --;
-        }
-
-        return $months;
-    }
-
-    /**
-     * Khởi tạo các thuộc tính dữ liệu
-     * 
+     * Khởi tạo dữ liệu
      * @return void 
      * @throws Exception 
      */
     protected function init(): void
     {
-        if (!$this->isLeap()) {
-            return;
+        $lastNewMoon = new Lunar11thNewMoonPhase(
+            $this->newMoon11th->getYear() - 1, 
+            $this->getOffset()
+        );
+
+        $counter = 0;
+        $result = [];
+        $nm = $lastNewMoon->add(1);
+        $getDeg = function (NewMoonPhaseInterface $newMoonPhase): int
+        {
+            $sl = new MjdToSunlongitude($newMoonPhase);
+            return floor($sl->getMidnightDegrees() / 30);
+        };
+
+        do {
+            $nextNm = $nm->add(1);
+            $deg = $getDeg($nm);
+            $nextDeg = $getDeg($nextNm);
+
+            if ($deg == $nextDeg) {
+                array_push($result, [
+                    'month' => $counter - 1,
+                    'jd'    => $nm->getJd()
+                ]);
+            }
+
+            $nm = $nextNm;
+
+            $counter ++;
+        } while ($counter < 13);
+
+        $counter = count($result);
+        
+        if ($counter === 0) {
+            throw new Exception("Error. Can not find the leap month.");
         }
 
-        $leaps = $this->collectMonthsCanBeLeap();
-        $counter = count($leaps);
-
-        if ($this->newMoon11th->getYear() === 2052) {
-            var_dump($leaps);
-        }
-
-        // Trường hợp năm có thể nhuận, nhưng không thể tìm được tháng nhuận, cần kiểm tra lại phương pháp tính
-        if ($counter == 0) {
-            throw new Exception("Error. Can not find lunar months can be leap.");
-        }
-
-        // Trường hợp tìm được nhiều hơn 1 kết quả, thì năm nhuận là tháng 11 Âm lịch
         if ($counter > 1) {
             $this->month = 11;
             $this->jd = $this->newMoon11th->add(1)->getJd();
         }
         else {
-            $item = end($leaps);
-            $this->month = $item['month'];
-            $this->jd = $item['new_moon']->getJd();
+            $this->month = $result[0]['month'];
+            $this->jd = $result[0]['jd'];
         }
-}
-    /**
-     * {@inheritdoc}
-     */
-    public function isLeap(): bool 
-    { 
-        return in_array(
-            $this->newMoon11th->getYear() % 19,
-            [0, 3, 6, 9, 11, 14, 17]
-        );
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getMonth(): int 
+    public function isLeap(): bool 
+    { 
+        return ($this->getMonth()) ? true : false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMonth(): null|int 
     { 
         return $this->month;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOffset(): int
+    {
+        return $this->newMoon11th->getOffset();
     }
 }
