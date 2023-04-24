9dae7e39f1d5c99c99319c5abb92dd5b65dc5730
vantran445 <CaoVan.Info@gmail.com>
2023-04-24 22:41:34 +0700

    Update class (incomplete)

--- src/Lunar/LunarDateTimeCorrector.php
+++ src/Lunar/LunarDateTimeCorrector.php
@@ -1,12 +1,24 @@
 <?php namespace VanTran\LunarCalendar\Lunar;
 
+use Exception;
 use VanTran\LunarCalendar\Mjd\MjdInterface;
+use VanTran\LunarCalendar\MoonPhases\BaseNewMoonPhase;
 use VanTran\LunarCalendar\MoonPhases\Lunar11thNewMoonPhase;
 use VanTran\LunarCalendar\MoonPhases\Lunar11thNewMoonPhaseInterface;
 use VanTran\LunarCalendar\MoonPhases\MoonPhaseInterface;
 
 class LunarDateTimeCorrector implements LunarBaseComponentInterface
 {
+    /**
+     * Năm tối thiểu hỗ trợ lập lịch
+     */
+    public const MIN_YEAR = 1901;
+
+    /**
+     * Năm tối đa hỗ trợ lập lịch
+     */
+    public const MAX_YEAR = 2100;
+
     /**
      * @var MjdInterface
      */
@@ -23,30 +35,87 @@ class LunarDateTimeCorrector implements LunarBaseComponentInterface
     protected $newMoon11th;
 
     /**
-     * @var LunarLeapMonthInterface
+     * @var null|LunarLeapMonthInterface
      */
     protected $leapMonth;
 
+    /**
+     * @var int Số ngày trong tháng Âm lịch
+     */
+    protected $dayOfMonth;
+
     public function __construct(protected LunarInputInterface $lunar)
     {
-        $this->correct();
+        $this->init();
     }
 
     /**
      * Thực hiện các bước tuần tự làm chính xác lại dữ liệu từ đầu vào
      * @return void 
      */
-    protected function correct(): void
+    protected function init(): void
     {
-        $this->correct11thNewMoon();
-        $this->correctLeapMonth();
+        // Xác thực năm đầu vào nằm trong khoảng thời gian cho phép
+        $this->validateYear();
+
+        // Khởi tạo được dữ liệu tháng 11 Âm lịch của năm
+        $this->init11thNewMoon();
+
+        // Khởi tạo và xác thực dữ liệu tháng nhuận
+        $this->initLeapMonth();
+        $this->validateLeapMonth();
+
+        // Khởi tạo điểm sóc đầu vào
+        $this->initNewMoon();
+
+
+    }
+    
+    /**
+     * Xác thực năm âm lịch đầu vào
+     * @return void 
+     * @throws Exception 
+     */
+    protected function validateYear(): void
+    {
+        $year = $this->lunar->getYear();
+
+        if ($year < self::MIN_YEAR || $year > self::MAX_YEAR) {
+            throw new Exception('Error. Support lunar year from ' . self::MIN_YEAR . ' to ' . self::MAX_YEAR);
+        }
     }
 
     /**
-     * Xác định điểm Sóc tháng 11 của năm Âm lịch cần tìm
-     * @return LunarDateTimeCorrector 
+     * Xác thực tháng nhuận đầu vào và tháng nhuận tính toán được
+     * 
+     * @return void 
+     * @throws Exception 
      */
-    protected function correct11thNewMoon(): void
+    protected function validateLeapMonth(): void
+    {
+        if ($this->lunar->isLeapMonth()) {
+            $leap = $this->getLeapMonth();
+
+            if ($leap == null) {
+                throw new Exception('Error. The Lunar year ' . $this->lunar->getYear() . ' dose not have leap month.');
+            }
+
+            if ($leap->getMonth() != $this->lunar->getMonth()) {
+                throw new Exception(sprintf(
+                    "Error. The leap month of lunar year %d is %d, not %d.",
+                    $this->lunar->getYear(),
+                    $leap->getMonth(),
+                    $this->lunar->getMonth()
+                ));
+            }
+        }
+    }
+
+    /**
+     * Khởi tạo điểm Sóc tháng 11 của năm Âm lịch cần tìm
+     * @return LunarDateTimeinitor 
+     */
+    protected function init11thNewMoon(): void
     {
         $this->newMoon11th = new Lunar11thNewMoonPhase(
             $this->lunar->getYear(), 
@@ -54,9 +123,60 @@ class LunarDateTimeCorrector implements LunarBaseComponentInterface
         );
     }
 
-    protected function correctLeapMonth(): void
+    /**
+     * Khởi tạo dữ liệu tháng nhuận âm lịch
+     * @return void 
+     */
+    protected function initLeapMonth(): void
     {
-        $this->leapMonth = new LunarLeapMonth($this->get11thNewMoon());
+        $leap = new LunarLeapMonth($this->get11thNewMoon());
+        
+        if ($leap->isLeap()) {
+            $this->leapMonth = $leap;
+        }
+    }
+
+    /**
+     * Khởi tạo dữ liệu điểm Sóc đầu vào
+     * 
+     * @return void 
+     */
+    protected function initNewMoon(): void
+    {
+        $month = $this->lunar->getMonth();
+        $isLeap = $this->lunar->isLeapMonth();
+        $leap = $this->getLeapMonth();
+
+        if ($month == 11) {
+            $this->newMoon = ($isLeap)
+                ? $this->get11thNewMoon()->add(1)
+                : $this->get11thNewMoon();
+
+            return;
+        }
+
+        if ($isLeap && $month == $leap->getMonth()) {
+            $this->newMoon = new BaseNewMoonPhase($leap->getJd(), $leap->getOffset());
+            return;
+        }
+
+        $k = $month - 11;
+
+        if ($leap) {
+            if ($k >= 0) {
+                if ($leap->getMonth() == 11) {
+                    if ($isLeap && $month == 11 || $month == 12) {
+                        $k += 1;
+                    }
+                }
+            } else {
+                if ($month <= $leap->getMonth() && $leap->getMonth() != 11) {
+                    $k -= 1;
+                }
+            }
+        }
+        
+        $this->newMoon = $this->get11thNewMoon()->add($k);
     }
 
     /**
@@ -91,4 +211,11 @@ class LunarDateTimeCorrector implements LunarBaseComponentInterface
         return $this->leapMonth;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getDayOfMonth(): int
+    {
+        return $this->dayOfMonth;
+    }
 }
