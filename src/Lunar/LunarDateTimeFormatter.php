011f7a9edaa1683f8282586cf666a5be49cce9a4
vantran445 <CaoVan.Info@gmail.com>
2023-04-25 21:39:26 +0700

    Cập nhật lớp định dạng thời gian Âm lịch

--- src/Lunar/LunarDateTimeFormatter.php
+++ src/Lunar/LunarDateTimeFormatter.php
@@ -1,5 +1,11 @@
 <?php namespace VanTran\LunarCalendar\Lunar;
 
+/**
+ * Lớp hỗ trợ định dạng thời gian Âm lịch
+ * 
+ * @author Văn Trần <caovan.info@gmail.com>
+ * @package VanTran\LunarCalendar\Lunar
+ */
 class LunarDateTimeFormatter
 {
     /**
@@ -8,15 +14,45 @@ class LunarDateTimeFormatter
      */
     protected $caching = [];
 
+    /**
+     * Danh sách các ký tự được hỗ trợ định dạng
+     * @var string[]
+     */
     protected $supported = [
-        'd', 'j', 'n', 'l', 'm', 'L', 'Y', 't'
+        'd', // Ngày trong tháng, 2 chữ số có số 0 đứng đầu
+        'j', // Ngày trong tháng không có số 0 đứng đầu
+        'm', // Biểu thị số của một tháng, có số 0 đứng đầu, nếu tháng nhuận hậu tố  có thêm ký tự '+' (dấu cộng)
+        'n', // Biểu thị số của một tháng, không có số 0 đứng đầu, nếu tháng nhuận hậu tố  có thêm ký tự '+' (dấu cộng)
+        'l', // Biểu thị số của một tháng, không có số 0 đứng đầu, không xác định tháng nhuận
+        'L', // Biểu thị số của một tháng, có số 0 đứng đầu, không xác định tháng nhuận
+        'Y', // Biểu thị năm đầy đủ gồm 4 chữ số
+        't', // Tổng số ngày trong tháng Âm lịch, có thể là 29 (tháng thiếu) hoặc 30 (tháng đủ)
+        'G', // Định dạng 24 giờ của một giờ không có số 0 đứng đầu
+        'g', // Định dạng 12 giờ của một giờ không có số 0 đứng đầu
+        'H', // Định dạng 24 giờ của một giờ có số 0 đứng đầu
+        'h', // Định dạng 12 giờ của một giờ có số 0 đứng đầu
+        'i', // Định dạng phút có số 0 đứng đầu
+        's', // Định dạng giây có số năm đứng đầu
+        'a', // Định dạng chữ thường (tiếng Anh) phân biệt Buổi sáng và Buổi chiều: am hoặc pm
+        'A', // Định dạng chữ HOA (tiếng Anh) phân biệt Buổi sáng và Buổi chiều: AM hoặc PM
+        'P', // Chênh lệch với giờ Greenwich (GMT) với dấu hai chấm giữa giờ và phút (+07:00)
+        'U', // Số giây kể từ Unix Epoch (ngày 1 tháng 1 năm 1970 00:00:00 GMT)
+        'Z', // Độ lệch múi giờ tính bằng giây
+        'C', // Chuỗi (+) xác định tháng nhuận - kết hợp khi sử dụng 'l' hoặc 'L' thay vì 'm' hoặc 'n'
     ];
 
-    public function __construct(protected LunarBaseComponentInterface $component)
-    {
-        
-    }
+    /**
+     * Tạo đối tượng mới
+     * @param LunarBaseComponentInterface $component Lớp chứa các thành phần tính toán Âm
+     * @return void 
+     */
+    public function __construct(protected LunarBaseComponentInterface $component) {}
 
+    /**
+     * Truy xuất các giá trị trong bộ đệm
+     * @param string $name 
+     * @return mixed 
+     */
     public function __get(string $name)
     {
         return isset($this->caching[$name])
@@ -24,6 +60,12 @@ class LunarDateTimeFormatter
             : null;
     }
 
+    /**
+     * Gán giá trị vào bộ đệm
+     * @param string $name 
+     * @param string $value 
+     * @return void 
+     */
     public function __set(string $name, string $value)
     {
         $this->caching[$name] = $value;
@@ -49,10 +91,15 @@ class LunarDateTimeFormatter
         return true;
     }
 
+    /**
+     * Định dạng thời gian Âm lịch
+     * @param string $formater 
+     * @return string 
+     */
     public function format(string $formater): string
     {
         foreach ($this->supported as $key) {
-            while(str_contains($formater, $key)) {
+            if(str_contains($formater, $key)) {
                 $formater = str_replace($key, $this->get($key), $formater);
             }
         }
@@ -60,6 +107,11 @@ class LunarDateTimeFormatter
         return $formater;
     }
 
+    /**
+     * Lấy giá trị từ một ký tự định dạng được hỗ trợ
+     * @param string $key 
+     * @return false|string 
+     */
     public function get(string $key): false|string
     {
         if ($value = $this->{$key}) {
@@ -93,7 +145,7 @@ class LunarDateTimeFormatter
                     $val = 11 - $phases;
 
                     if ($leap->isLeap()) {
-                        if (!$this->isCurrentLeapMonth() && $val < $leap->getMonth()) {
+                        if (!$this->isCurrentLeapMonth() && $val < $leap->getMonth() && $leap->getMonth() != 11) {
                             $val --;
                         }
                     }
@@ -120,6 +172,55 @@ class LunarDateTimeFormatter
 
             case 't':
                 $val = $comp->getDayOfMonth();
+                break;
+
+            case 'G':
+                $val = round($comp->getDiffJd() * 24 % 24, 0, PHP_ROUND_HALF_DOWN);
+                break;
+
+            case 'g':
+                $val = ($this->get('G') > 12)? $this->get('G') % 12 : $this->get('G');
+                break;
+
+            case 'H':
+                $val = str_pad($this->get('G'), 2, '0', STR_PAD_LEFT);
+                break;
+
+            case 'h':
+                $val = str_pad($this->get('g'), 2, '0', STR_PAD_LEFT);
+                break;
+
+            case 'i':
+                $val = round($comp->getDiffJd() * 60 * 24, PHP_ROUND_HALF_DOWN) % 60;
+                $val = str_pad($val, 2, '0', STR_PAD_LEFT);
+                break;
+
+            case 's':
+                $val = round($comp->getDiffJd() * 3600 * 24) % 60;
+                $val = str_pad($val, 2, '0', STR_PAD_LEFT);
+                break;
+            
+            case 'a':
+                $val = ((int)$this->get('G') < 12)? 'am' : 'pm';
+                break;
+
+            case 'A':
+                $val = ((int)$this->get('G') < 12)? 'AM' : 'PM';
+                break;
+
+            case 'P':
+                return $comp->getTimeZone()->getName();
+
+            case 'U':
+                $val = ($comp->getJd() - $comp::EPOCH_MJD) * 86400;
+                $val = round($val);
+                break;
+
+            case 'Z': 
+                return $comp->getOffset();
+
+            case 'C':
+                $val = ($this->isCurrentLeapMonth())? '(+)' : '';
                 break;
             
             default:
