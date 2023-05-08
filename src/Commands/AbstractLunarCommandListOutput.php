2ff96fd3d9ec997a440baf1dc03be96f486a0e60
vantran445 <CaoVan.Info@gmail.com>
2023-05-08 19:11:35 +0700

    Cập nhật gói Lunar CLI
    - Tái cấu trúc
    - Lớp trừu tượng cho các lệnh tạo danh sách dữ liệu
    - Lớp tạo danh sách Pha trăng mới

---
+++ src/Commands/AbstractLunarCommandListOutput.php
@@ -0,0 +1,241 @@
+<?php namespace VanTran\LunarCalendar\Commands;
+
+use DivisionByZeroError;
+use ArithmeticError;
+use Brick\VarExporter\ExportException;
+use Brick\VarExporter\VarExporter;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Exception\LogicException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Lớp trừu tượng hỗ trợ các lệnh tạo, kết xuất danh sách dữ liệu ở cấp độ đơn giản
+ * 
+ * @author Văn Trần <caovan.info@gmail.com>
+ * @package VanTran\LunarCalendar\Commands
+ */
+abstract class AbstractLunarCommandListOutput extends Command
+{
+    /**
+     * @var bool Phân tách các hàng bảng đầu ra
+     */
+    protected $tableSeparated = true;
+
+    /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    protected $formatter;
+
+    /**
+     * Trả về mảng chứa các tiêu đề cột bảng thông tin
+     * 
+     * @return array 
+     */
+    abstract protected function getInfoTableHeader(): array;
+
+    /**
+     * Trả về mảng chứa các thông tin hỗ trợ
+     * 
+     * @return array 
+     */
+    abstract protected function getInformationData(): array;
+
+    /**
+     * Trả về mảng chứa các tiêu đề bảng dữ liệu
+     * 
+     * @return array 
+     */
+    abstract protected function getDataTableHeader(): array;
+
+    /**
+     * Trả về mảng danh sách dữ liệu
+     * 
+     * @return array 
+     */
+    abstract protected function getListData(): array;
+
+    /**
+     * Trả về phân loại kết xuất đầu ra, hỗ trợ (1) với định dạng bảng. (2) cho định dạng mảng PHP.
+     * @return int 
+     */
+    abstract protected function getOuputType(): int;
+
+    /**
+     * Các lớp mở rộng có thể ghi đè phương thức này để thực hiện việc xác thực đầu vào
+     * @return int 
+     */
+    protected function validate(): int
+    {
+        return 0;
+    }
+
+    /**
+     * Thêm định dạng phân tách các hàng trong bảng
+     * 
+     * @param mixed $data 
+     * @return void 
+     */
+    private function insertTableSeperator(&$data): void
+    {
+        $modifiedData = [];
+        $total = count($data);
+
+        foreach ($data as $index => $item) {
+            $modifiedData[] = $item;
+
+            if ($index != $total - 1) {
+                $modifiedData[] = new TableSeparator();
+            }
+        }
+
+        $data = $modifiedData;
+    }
+
+    /**
+     * Kết xuất bảng thông tin lệnh
+     * 
+     * @return void 
+     * @throws InvalidArgumentException 
+     * @throws DivisionByZeroError 
+     * @throws ArithmeticError 
+     */
+    protected function renderInfoTable(): void
+    {
+        if (empty($data = $this->getInformationData())) {
+            return;
+        }
+
+        if ($this->tableSeparated) {
+            $this->insertTableSeperator($data);
+        }
+
+        $header = $this->getInfoTableHeader();
+        $table = new Table($this->output);
+
+        if (!empty($header)) {
+            $table->setHeaders($header);
+        }
+        
+        $table->setHeaderTitle("Cấu hình lệnh");
+        $table->setRows($data);
+        $table->render();
+
+        $this->output->writeln('');
+    }
+
+    /**
+     * Kết xuất bảng dữ liệu đầu ra
+     * 
+     * @return void 
+     * @throws InvalidArgumentException 
+     * @throws DivisionByZeroError 
+     * @throws ArithmeticError 
+     */
+    protected function renderDataTable(): void
+    {
+        $data = $this->getListData();
+
+        if (empty($data)) {
+            return;
+        }
+
+        if ($this->tableSeparated) {
+            $this->insertTableSeperator($data);
+        }
+
+        $header = $this->getDataTableHeader();
+        $table = new Table($this->output);
+
+        if (!empty($header)) {
+            $table->setHeaders($header);
+        }
+
+        $table->setHeaderTitle("Danh sách dữ liệu");
+        $table->setRows($data);
+        $table->render();
+    }
+
+    /**
+     * Kết xuất đầu ra dưới dạng mảng PHP
+     * 
+     * @return void 
+     * @throws ExportException 
+     */
+    protected function renderPhpArray(): void
+    {
+        $data = $this->getListData();
+        $content = 'return ' . VarExporter::export($data) . ';';
+        
+        $this->output->writeln('<comment>' .$content . '</comment>');
+        $this->output->writeln('');
+    }
+
+    /**
+     * Chuẩn bị dữ liệu trước khi thực thi lệnh
+     * 
+     * @param InputInterface $input 
+     * @param OutputInterface $output 
+     * @return void 
+     * @throws LogicException 
+     * @throws InvalidArgumentException 
+     */
+    private function prepareExecuting(InputInterface $input, OutputInterface $output): void
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->formatter = $this->getHelper('formatter');
+    }
+
+    protected function printErrorMasage($message): void
+    {
+        $this->output->writeln(
+            $this->formatter->formatBlock($message, 'error', true)
+        );
+
+        $this->output->writeln('');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->prepareExecuting($input, $output);
+        $this->renderInfoTable();
+
+        $validationResult = $this->validate();
+
+        if ($validationResult != 0) {
+            return $validationResult;
+        }
+
+        switch ($this->getOuputType()) {
+            case 1:
+                $this->renderDataTable();
+                break;
+
+            case 2: 
+                $this->renderPhpArray();
+                break;
+
+            default:
+                $this->printErrorMasage('Lỗi. Tùy chọn định dạng xuất ra không hợp lệ.');
+                return Command::INVALID;
+        }
+
+        $this->output->writeln('');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Commands/CreateNewMoonPhaseList.php
@@ -0,0 +1,168 @@
+<?php namespace VanTran\LunarCalendar\Commands;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputOption;
+use VanTran\LunarCalendar\Commands\Traits\HasDateTimeOption;
+use VanTran\LunarCalendar\Converters\BaseNewMoonPhaseConverter;
+use VanTran\LunarCalendar\Converters\DateTimeToJdnConverter;
+use VanTran\LunarCalendar\Converters\JdnToDateTimeConverter;
+
+#[AsCommand(
+    name: 'create:new-moon-phase-list',
+    description: 'Tạo danh sách các điểm Trăng mới (Sóc) từ một mốc thời gian dương lịch',
+    hidden: false,
+    aliases: [
+        'make-nmp-list'
+    ]
+)]
+
+class CreateNewMoonPhaseList extends AbstractLunarCommandListOutput
+{
+    use HasDateTimeOption;
+
+    /**
+     * @inheritdoc
+     */
+    protected function getInfoTableHeader(): array 
+    { 
+        return [
+            'Thiết lập đầu vào',
+            'Giá trị'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getInformationData(): array 
+    { 
+        return [
+            [
+                'Mô tả',
+                $this->getDescription(),
+            ],
+            [
+                'Thời gian đầu vào',
+                $this->getInputDateTime()->format('Y-m-d H:i:s')
+            ],
+            [
+                'Múi giờ địa phương',
+                $this->getInputTimeZone()->getName(),
+            ],
+            [
+                'Kết quả đầu ra',
+                $this->input->getOption('quantity')
+            ],
+            [
+                'Sắp xếp kết quả',
+                ($this->input->getOption('next')) ? '(1) Tìm điểm Sóc kế tiếp' : '(2) Tìm điểm Sóc trước đó'
+            ],
+            [
+                'Định dạng đầu ra',
+                ($this->input->getOption('output-type') == 1) ? '(1) Bảng dữ liệu' : '(2) Mảng PHP'
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getDataTableHeader(): array 
+    { 
+        return [
+            'Số ngày MJD',
+            'Tổng chu kỳ Trăng',
+            'Thời gian bắt đầu'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getListData(): array 
+    { 
+        $jdn = new DateTimeToJdnConverter($this->getInputDateTime());
+        $newMoon = new BaseNewMoonPhaseConverter(
+            $jdn->getJd(), 
+            $jdn->getOffset()
+        );
+
+        $items = [];
+
+        for ($i = 1; $i <= $this->input->getOption('quantity'); $i ++) 
+        {
+            $ouputDate = (new JdnToDateTimeConverter($newMoon))->getDateTime();
+
+            array_push($items, [
+                'jd' => $newMoon->getJd(),
+                'cycles' => $newMoon->getTotalCycles(),
+                'date' => $ouputDate->format('Y-m-d H:i:s P'),
+            ]);
+
+            $newMoon = ($this->input->getOption('next'))
+                ? $newMoon->add(1)
+                : $newMoon->subtract(1);
+        }
+
+        return $items;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getOuputType(): int 
+    { 
+        return $this->input->getOption('output-type');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setHelp("Cho phép bạn tạo ra một danh sách các điểm của Pha trăng mới (điểm Sóc) sắp xếp theo thứ tự.");
+
+        // Input argumments
+        $this->addOption(
+            'date', 
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Mốc thời gian bắt đầu',
+            ''
+        );
+
+        $this->addOption(
+            'timezone',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Múi giờ địa phương (định danh), mặc định sử dụng GMT+7 cho Âm lịch Việt Nam',
+            '+0700'
+        );
+        
+
+        // Options
+        $this->addOption(
+            'quantity',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Số lượng kết quả đầu ra mong muốn',
+            10
+        );
+
+        $this->addOption(
+            'next',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Kết quả tiếp theo được lấy ở mốc thời gian trong tương lai (điểm kế tiếp)',
+            1
+        );
+
+        $this->addOption(
+            'output-type',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Tùy chọn xuất ra bảng dữ liệu (mặc định) hoặc cấu trúc mảng PHP.',
+            1
+        );
+    }
+}
