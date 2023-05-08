0c49c3e60c0c8e186857aee469a6edd5d31d0da5
vantran445 <CaoVan.Info@gmail.com>
2023-05-08 14:28:21 +0700

    Thêm bộ chuyển đổi Julian thành lịch Gregorian có tham số chênh lệch giờ địa phương

---
+++ tests/Converters/JdnToLocalGregorianTest.php
@@ -0,0 +1,44 @@
+<?php namespace VanTran\LunarCalendar\Tests\Converters;
+
+use PHPUnit\Framework\TestCase;
+use VanTran\LunarCalendar\Converters\BaseJdnToGregorian;
+use VanTran\LunarCalendar\Converters\GregorianToJDNConverter;
+use VanTran\LunarCalendar\Converters\JdnToLocalGregorian;
+use VanTran\LunarCalendar\Interfaces\JulianDayNumberInterface;
+
+class JdnToLocalGregorianTest extends TestCase
+{
+    public function testOuput(): void
+    {
+        $year = 2023;
+        $month = 10;
+        $day = 2;
+        $hour = 6;
+        $offset = 25200; // Việt Nam
+
+        $jd = new GregorianToJDNConverter(
+            $year,
+            $month,
+            $day,
+            $hour,
+            1,
+            1,
+            $offset
+        );
+
+        $localCvt = new JdnToLocalGregorian($jd);
+
+        // UTC tương ứng
+        $utcCvt = new BaseJdnToGregorian($jd->getJd());
+
+        $this->assertEquals($year, $localCvt->getYear());
+        $this->assertEquals($month, $localCvt->getMonth());
+        $this->assertEquals($day, $localCvt->getDay());
+        $this->assertEquals($hour, $localCvt->getHour());
+        $this->assertEquals('+07:00', $localCvt->getTimezone()->getName());
+
+        // 6 giờ sáng tại Việt Nam tương ứng với 23 giờ đêm ngày hôm trước theo UTC
+        $this->assertEquals($day - 1, $utcCvt->getDay());
+        $this->assertEquals(23, $utcCvt->getHour());
+    }
+}
