3b9f2d4e334b8d9052cc3b126fa30a97b90eb5c5
vantran445 <CaoVan.Info@gmail.com>
2023-05-10 20:32:44 +0700

    Create SexagenariesInterface.php

---
+++ src/Interfaces/SexagenariesInterface.php
@@ -0,0 +1,26 @@
+<?php namespace VanTran\LunarCalendar\Interfaces;
+
+/**
+ * Giao diện cho các lớp quản lý, chuyển đổi và truy xuất giá trị của hệ thống Can, Chi.
+ * 
+ * @author Văn Trần <caovan.info@gmail.com>
+ * @package VanTran\LunarCalendar\Interfaces
+ */
+interface SexagenariesInterface
+{
+    /**
+     * Trả về số chỉ mục của hệ thống Can Chi.
+     *  
+     * @param string $char 
+     * @return null|int 
+     */
+    public function getIndex(string $key): string|int;
+
+    /**
+     * Trả về ký tự đại diện của Can hoặc Chi
+     * 
+     * @param string $key 
+     * @return string 
+     */
+    public function getCharacter(string $key): string;
+}
