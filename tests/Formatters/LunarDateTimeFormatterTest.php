454cd50e433dbdfe6e0aeeab35165935f6a20705
vantran445 <CaoVan.Info@gmail.com>
2023-05-10 14:07:57 +0700

    Thêm lớp định dạng đầu ra ngày tháng Âm lịch

---
+++ tests/Formatters/LunarDateTimeFormatterTest.php
@@ -0,0 +1,39 @@
+<?php namespace VanTran\LunarCalendar\Tests\Formatters;
+
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use VanTran\LunarCalendar\Correctors\GregorianToLunarCorrector;
+use VanTran\LunarCalendar\Correctors\LunarDateTimeCorrector;
+use VanTran\LunarCalendar\Formatters\LunarDateTimeFormatter;
+use VanTran\LunarCalendar\Parsers\LunarDateTimeParser;
+use VanTran\LunarCalendar\Storages\GregorianToLunarStorageMutable;
+
+class LunarDateTimeFormatterTest extends TestCase
+{
+    public function testNoLeapMonth(): void
+    {
+        $formatter = new LunarDateTimeFormatter(
+            new LunarDateTimeCorrector(
+                new LunarDateTimeParser('21/03/2023 19:00 +0700')
+            )
+        );
+
+        $this->assertEquals('2023-3-21 07:00pm', $formatter->format('Y-l-d h:ia'));
+        $this->assertEquals('21/03/2023 19:00 +07:00', $formatter->format('d/m/Y H:i P'));
+    }
+
+    public function testHasLeapMonth(): void
+    {
+        $formater = new LunarDateTimeFormatter(
+            new GregorianToLunarCorrector(
+                new GregorianToLunarStorageMutable(
+                    new DateTime('2023-04-19 00:00 +0700')
+                )
+            )
+        );
+
+        $this->assertEquals('29/02/2023 00:00am', $formater->format('d/L/Y h:ia'));
+        $this->assertEquals('29/02+/2023', $formater->format('d/m/Y'));
+        $this->assertEquals('29/2/2023 (+)', $formater->format('d/l/Y C'));
+    }
+}
