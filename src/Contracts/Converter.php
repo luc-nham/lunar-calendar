91f0a3d8d45983f063873581cc0c302132a14995
vantran445 <admin@lucnham.com>
2024-09-26 13:45:31 +0700

    Add Converter interface

---
+++ src/Contracts/Converter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LucNham\LunarCalendar\Contracts;
+
+use Closure;
+
+/**
+ * Definition for converters
+ */
+interface Converter
+{
+    /**
+     * Return the ouput value
+     */
+    public function getOuput();
+
+    /**
+     * Forward the output to a callback function
+     * 
+     * @param $cb (($output): mixed)
+     * @return mixed
+     */
+    public function forward(Closure $cb);
+}
