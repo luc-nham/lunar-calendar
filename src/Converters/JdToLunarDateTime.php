cdc7a43042834499a31bb62adec6748a7c35c8fc
vantran445 <admin@lucnham.com>
2024-10-12 09:58:05 +0700

    Add class coverts Julian day number to Lunar date time interval

---
+++ src/Converters/JdToLunarDateTime.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace LucNham\LunarCalendar\Converters;
+
+use LucNham\LunarCalendar\Converters\Traits\JdInputSetable;
+use LucNham\LunarCalendar\Terms\LunarDateTimeInterval;
+use LucNham\LunarCalendar\Terms\LunarLeapMonthNewMoonPhase;
+use LucNham\LunarCalendar\Terms\NewMoonPhase;
+
+/**
+ * Converter that converts a Julian day number to Lunar date time period.
+ */
+class JdToLunarDateTime extends Converter
+{
+    use JdInputSetable;
+
+    /**
+     * Create new converter
+     *
+     * @param float $jd         Julian day number input, default 2440623.5 is corresponding to Lunar
+     *                          1970-01-01T00:00+0000
+     * @param integer $offset   UTC offset in seconds, default 0 mean UTC
+     */
+    public function __construct(private float $jd = 2440623.5, int $offset = 0)
+    {
+        $this->setOffset($offset);
+    }
+
+    /**
+     * Get the Lunar month number
+     *
+     * @param NewMoonPhase $nm                          Current (input) new moon phase
+     * @param NewMoonPhase $fnm                         Lunar first new moon phase
+     * @param LunarLeapMonthNewMoonPhase|null $lnm      Lunar leap month new moon phase
+     * @return int
+     */
+    protected function getMonthNumber(
+        NewMoonPhase $nm,
+        NewMoonPhase $fnm,
+        ?LunarLeapMonthNewMoonPhase $lnm
+    ): int {
+        if ($nm->total === $fnm->total) {
+            return 1;
+        }
+
+        $month = $nm->total - $fnm->total;
+
+        if (!$lnm || $nm->total < $lnm->total) {
+            return $month + 1;
+        }
+
+        return $month;
+    }
+
+    /**
+     * Return Lunar date time period.
+     *
+     * @return LunarDateTimeInterval
+     */
+    public function getOutput(): LunarDateTimeInterval
+    {
+        $offset = $this->offset();
+        $jd = $this->jd;
+        $mjd = (new JdToMidnightJd($jd, $offset))->getOutput();
+        $newMoon = (new JdToLunarNewMoon($mjd, $offset))->getOutput();
+        $fistNewMoon = (new NewMoonToLunarFirstNewMoon($newMoon, $offset))->getOutput();
+        $leapNewMoon = (new LunarFirstNewMoonToLunarLeapNewMoon($fistNewMoon, $offset))->getOutput();
+        $time = (new JdToTime($this->jd, $offset))->getOutput();
+
+        return new LunarDateTimeInterval(
+            d: floor($mjd - $newMoon->jd + 1),
+            m: $this->getMonthNumber($newMoon, $fistNewMoon, $leapNewMoon),
+            y: $fistNewMoon->year,
+            l: $leapNewMoon?->month ?? 0,
+            leap: $newMoon->total === $leapNewMoon?->total,
+            h: $time->h,
+            i: $time->i,
+            s: $time->s
+        );
+    }
+}
---
+++ tests/Converters/JdToLunarDateTimeTest.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace LucNham\LunarCalendar\Tests\Converters;
+
+use LucNham\LunarCalendar\Converters\GregorianToJd;
+use LucNham\LunarCalendar\Converters\JdToGregorian;
+use LucNham\LunarCalendar\Converters\JdToLs;
+use LucNham\LunarCalendar\Converters\JdToLunarDateTime;
+use LucNham\LunarCalendar\Converters\JdToLunarNewMoon;
+use LucNham\LunarCalendar\Converters\JdToMidnightJd;
+use LucNham\LunarCalendar\Converters\JdToTime;
+use LucNham\LunarCalendar\Converters\LunarFirstNewMoonToLunarLeapNewMoon;
+use LucNham\LunarCalendar\Converters\NewMoonIterator;
+use LucNham\LunarCalendar\Converters\NewMoonToLunarFirstNewMoon;
+use LucNham\LunarCalendar\Terms\DateTimeInterval;
+use LucNham\LunarCalendar\Terms\LunarDateTimeInterval;
+use LucNham\LunarCalendar\Terms\LunarFirstNewMoonPhase;
+use LucNham\LunarCalendar\Terms\LunarLeapMonthNewMoonPhase;
+use LucNham\LunarCalendar\Terms\NewMoonPhase;
+use LucNham\LunarCalendar\Terms\TimeInterval;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(JdToLunarDateTime::class)]
+#[CoversClass(JdToTime::class)]
+#[CoversClass(TimeInterval::class)]
+#[CoversClass(JdToGregorian::class)]
+#[CoversClass(JdToLunarNewMoon::class)]
+#[CoversClass(JdToMidnightJd::class)]
+#[CoversClass(LunarFirstNewMoonToLunarLeapNewMoon::class)]
+#[CoversClass(NewMoonToLunarFirstNewMoon::class)]
+#[CoversClass(DateTimeInterval::class)]
+#[CoversClass(LunarDateTimeInterval::class)]
+#[CoversClass(LunarFirstNewMoonPhase::class)]
+#[CoversClass(NewMoonPhase::class)]
+#[CoversClass(GregorianToJd::class)]
+#[CoversClass(JdToLs::class)]
+#[CoversClass(NewMoonIterator::class)]
+#[CoversClass(LunarLeapMonthNewMoonPhase::class)]
+class JdToLunarDateTimeTest extends TestCase
+{
+    public function testDefault()
+    {
+        $converter = new JdToLunarDateTime(offset: 0);
+        $lunar = $converter->getOutput();
+
+        $this->assertEquals(1, $lunar->d);
+        $this->assertEquals(1, $lunar->m);
+        $this->assertEquals(1970, $lunar->y);
+        $this->assertEquals(0, $lunar->h);
+        $this->assertEquals(0, $lunar->i);
+        $this->assertEquals(0, $lunar->s);
+        $this->assertEquals(false, $lunar->leap);
+        $this->assertEquals(0, $lunar->l);          // 1970 is not a Lunar leap year
+
+        $converter->setOffset(25200);
+        $lunar = $converter->getOutput();
+
+        $this->assertEquals(1, $lunar->d);
+        $this->assertEquals(1, $lunar->m);
+        $this->assertEquals(1970, $lunar->y);
+        $this->assertEquals(7, $lunar->h);
+        $this->assertEquals(0, $lunar->i);
+        $this->assertEquals(0, $lunar->s);
+        $this->assertEquals(false, $lunar->leap);
+        $this->assertEquals(0, $lunar->l);          // 1970 is not a Lunar leap year
+    }
+
+    public function testBeforeLeapMonth()
+    {
+        /** @var LunarDateTimeInterval */
+        $lunar = ((new GregorianToJd(
+            g: new DateTimeInterval(
+                d: 21,
+                m: 3,
+                y: 2023,
+                h: 22,
+                i: 50,
+                s: 59
+            ),
+            offset: 25200
+        )))
+            ->then(JdToLunarDateTime::class)
+            ->getOutput();
+
+        $this->assertEquals(30, $lunar->d);
+        $this->assertEquals(2, $lunar->m);
+        $this->assertEquals(2023, $lunar->y);
+        $this->assertEquals(22, $lunar->h);
+        $this->assertEquals(50, $lunar->i);
+        $this->assertEquals(59, $lunar->s);
+        $this->assertEquals(false, $lunar->leap);
+        $this->assertEquals(2, $lunar->l);
+    }
+
+    public function testAfterLeapMonth()
+    {
+        /** @var LunarDateTimeInterval */
+        $lunar = ((new GregorianToJd(
+            g: new DateTimeInterval(
+                d: 18,
+                m: 2,
+                y: 2034,
+                h: 1,
+                i: 1,
+                s: 57
+            ),
+            offset: 25200
+        )))
+            ->then(JdToLunarDateTime::class)
+            ->getOutput();
+
+        $this->assertEquals(30, $lunar->d);
+        $this->assertEquals(12, $lunar->m);
+        $this->assertEquals(2033, $lunar->y);
+        $this->assertEquals(1, $lunar->h);
+        $this->assertEquals(1, $lunar->i);
+        $this->assertEquals(57, $lunar->s);
+        $this->assertEquals(false, $lunar->leap);
+        $this->assertEquals(11, $lunar->l);
+    }
+
+    public function testInLeapMonth()
+    {
+        /** @var LunarDateTimeInterval */
+        $lunar = ((new GregorianToJd(
+            g: new DateTimeInterval(
+                d: 24,
+                m: 10,
+                y: 2014,
+                h: 22,
+                i: 49,
+                s: 56
+            ),
+            offset: 25200
+        )))
+            ->then(JdToLunarDateTime::class)
+            ->getOutput();
+
+        $this->assertEquals(1, $lunar->d);
+        $this->assertEquals(9, $lunar->m);
+        $this->assertEquals(2014, $lunar->y);
+        $this->assertEquals(22, $lunar->h);
+        $this->assertEquals(49, $lunar->i);
+        $this->assertEquals(56, $lunar->s);
+        $this->assertEquals(true, $lunar->leap);
+        $this->assertEquals(9, $lunar->l);
+    }
+}
