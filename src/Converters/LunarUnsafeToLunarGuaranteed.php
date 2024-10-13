00fc7f4f22b52343a6d397718970995fd357692d
vantran445 <admin@lucnham.com>
2024-10-13 20:15:56 +0700

    Add class to converts unsafe Lunar date time interval to guaranteed Lunar date time

---
+++ src/Converters/LunarUnsafeToLunarGuaranteed.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LucNham\LunarCalendar\Converters;
+
+use LucNham\LunarCalendar\Terms\LunarDateTimeInterval;
+
+/**
+ * Correct unsafe Lunar date time input to safe Lunar date time output.
+ */
+class LunarUnsafeToLunarGuaranteed extends Converter
+{
+    /**
+     * Create new converter
+     *
+     * @param LunarDateTimeInterval $lunar  Lunar date time interval
+     * @param integer $offset               Timezone offset in seconds, default 0 mean UTC
+     */
+    public function __construct(private LunarDateTimeInterval $lunar, int $offset = 0)
+    {
+        $this->setOffset($offset);
+    }
+
+    /**
+     * Return corrected lunar date time:
+     * - Fix incorrect date and time input. For example, if input day is 35, the output with be 5 or
+     *   6 depends on total days of lunar month, also the month number output will be increase by 1.
+     * - Fix incorrect leap month input
+     *
+     * @return LunarDateTimeInterval
+     */
+    public function getOutput(): LunarDateTimeInterval
+    {
+        return (new LunarDateTimeToJd(
+            lunar: $this->lunar,
+            offset: $this->offset()
+        ))
+            ->then(JdToLunarDateTime::class)
+            ->getOutput();
+    }
+}
---
+++ tests/Converters/LunarUnsafeToLunarGuaranteedTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace LucNham\LunarCalendar\Tests\Converters;
+
+use LucNham\LunarCalendar\Converters\GregorianToJd;
+use LucNham\LunarCalendar\Converters\JdToGregorian;
+use LucNham\LunarCalendar\Converters\JdToLs;
+use LucNham\LunarCalendar\Converters\JdToLunarDateTime;
+use LucNham\LunarCalendar\Converters\JdToLunarNewMoon;
+use LucNham\LunarCalendar\Converters\JdToMidnightJd;
+use LucNham\LunarCalendar\Converters\JdToTime;
+use LucNham\LunarCalendar\Converters\LunarDateTimeToJd;
+use LucNham\LunarCalendar\Converters\LunarFirstNewMoonToLunarLeapNewMoon;
+use LucNham\LunarCalendar\Converters\LunarUnsafeToLunarGuaranteed;
+use LucNham\LunarCalendar\Converters\NewMoonIterator;
+use LucNham\LunarCalendar\Converters\NewMoonToLunarFirstNewMoon;
+use LucNham\LunarCalendar\Terms\DateTimeInterval;
+use LucNham\LunarCalendar\Terms\LunarDateTimeInterval;
+use LucNham\LunarCalendar\Terms\LunarFirstNewMoonPhase;
+use LucNham\LunarCalendar\Terms\LunarLeapMonthNewMoonPhase;
+use LucNham\LunarCalendar\Terms\NewMoonPhase;
+use LucNham\LunarCalendar\Terms\TimeInterval;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(LunarUnsafeToLunarGuaranteed::class)]
+#[CoversClass(JdToLunarDateTime::class)]
+#[CoversClass(JdToTime::class)]
+#[CoversClass(TimeInterval::class)]
+#[CoversClass(JdToGregorian::class)]
+#[CoversClass(JdToLunarNewMoon::class)]
+#[CoversClass(JdToMidnightJd::class)]
+#[CoversClass(LunarFirstNewMoonToLunarLeapNewMoon::class)]
+#[CoversClass(NewMoonToLunarFirstNewMoon::class)]
+#[CoversClass(DateTimeInterval::class)]
+#[CoversClass(LunarDateTimeInterval::class)]
+#[CoversClass(LunarFirstNewMoonPhase::class)]
+#[CoversClass(NewMoonPhase::class)]
+#[CoversClass(GregorianToJd::class)]
+#[CoversClass(JdToLs::class)]
+#[CoversClass(NewMoonIterator::class)]
+#[CoversClass(LunarLeapMonthNewMoonPhase::class)]
+#[CoversClass(LunarDateTimeToJd::class)]
+class LunarUnsafeToLunarGuaranteedTest extends TestCase
+{
+    public function testIncorrectDayNumber()
+    {
+        $lunar = (new LunarUnsafeToLunarGuaranteed(
+            lunar: new LunarDateTimeInterval(
+                d: 35,
+                m: 12,
+                y: 1970
+            ),
+            offset: 25200
+        ))->getOutput();
+
+        $this->assertEquals(5, $lunar->d);
+        $this->assertEquals(1, $lunar->m);
+        $this->assertEquals(1971, $lunar->y);
+    }
+
+    public function testIncorrectMonthNumber()
+    {
+        $lunar = (new LunarUnsafeToLunarGuaranteed(
+            lunar: new LunarDateTimeInterval(
+                d: 1,
+                m: 13,
+                y: 1970
+            ),
+            offset: 25200
+        ))->getOutput();
+
+        $this->assertEquals(1, $lunar->d);
+        $this->assertEquals(1, $lunar->m);
+        $this->assertEquals(1971, $lunar->y);
+    }
+
+    public function testIncorrectLeapMonthCheck()
+    {
+        $lunar = (new LunarUnsafeToLunarGuaranteed(
+            lunar: new LunarDateTimeInterval(
+                d: 3,
+                m: 5,
+                y: 1974,
+                leap: true
+            ),
+            offset: 25200
+        ))->getOutput();
+
+        $this->assertEquals(3, $lunar->d);
+        $this->assertEquals(5, $lunar->m);
+        $this->assertEquals(1974, $lunar->y);
+        $this->assertEquals(4, $lunar->l);
+        $this->assertFalse($lunar->leap);
+    }
+
+    public function testIncorrectSeconds()
+    {
+        $lunar = (new LunarUnsafeToLunarGuaranteed(
+            lunar: new LunarDateTimeInterval(
+                d: 30,
+                m: 4,
+                y: 1974,
+                h: 23,
+                i: 59,
+                s: 60
+            ),
+            offset: 25200
+        ))->getOutput();
+
+        $this->assertEquals(1, $lunar->d);
+        $this->assertEquals(4, $lunar->m);
+        $this->assertEquals(1974, $lunar->y);
+        $this->assertEquals(0, $lunar->h);
+        $this->assertEquals(0, $lunar->i);
+        $this->assertEquals(0, $lunar->s);
+        $this->assertEquals(4, $lunar->l);
+        $this->assertTrue($lunar->leap);
+    }
+}
