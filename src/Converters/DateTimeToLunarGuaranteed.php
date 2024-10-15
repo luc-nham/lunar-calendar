46d76f35365e77588d4f0f5a0bdd0d0b07a82e3e
vantran445 <admin@lucnham.com>
2024-10-15 18:48:25 +0700

    Add class to converts date time object to Lunar guaranteed date time

---
+++ src/Converters/DateTimeToLunarGuaranteed.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace LucNham\LunarCalendar\Converters;
+
+use DateTimeInterface;
+use DateTimeZone;
+use LucNham\LunarCalendar\Contracts\ZoneAccessible;
+use LucNham\LunarCalendar\Terms\DateTimeInterval;
+
+/**
+ * Convert a object that implement DateTimeInterface to Lunar date time
+ */
+class DateTimeToLunarGuaranteed extends GregorianToLunarDateTime implements ZoneAccessible
+{
+    /**
+     * Create new converter
+     *
+     * @param DateTimeInterface $datetime
+     */
+    public function __construct(private DateTimeInterface $datetime)
+    {
+        parent::__construct(
+            gregorian: new DateTimeInterval(
+                d: $datetime->format('j'),
+                m: $datetime->format('n'),
+                y: $datetime->format('Y'),
+                h: $datetime->format('G'),
+                i: (int)$datetime->format('i'),
+                s: (int)$datetime->format('s')
+            ),
+            offset: $datetime->getOffset()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTimezone(): DateTimeZone
+    {
+        return $this->datetime->getTimezone();
+    }
+}
