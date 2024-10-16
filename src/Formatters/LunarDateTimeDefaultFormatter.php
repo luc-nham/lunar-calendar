d106bd9e5122ec433c3a7a3c97d0b72661b338fe
vantran445 <admin@lucnham.com>
2024-10-16 12:43:26 +0700

    Add default lunar date time formatter

---
+++ src/Formatters/LunarDateTimeDefaultFormatter.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace LucNham\LunarCalendar\Formatters;
+
+use DateTimeZone;
+use LucNham\LunarCalendar\Contracts\LunarDateTimeFormattable;
+use LucNham\LunarCalendar\Converters\JdToLunarNewMoon;
+use LucNham\LunarCalendar\Converters\NewMoonIterator;
+use LucNham\LunarCalendar\Terms\LunarDateTimeGuaranteed;
+
+/**
+ * Default lunar date time formatter
+ */
+class LunarDateTimeDefaultFormatter implements LunarDateTimeFormattable
+{
+    public function __construct(
+        private LunarDateTimeGuaranteed $lunar,
+        private DateTimeZone $timezone,
+        private int $offset
+    ) {}
+
+    /**
+     * 't' formated
+     *
+     * @return void
+     */
+    protected function t()
+    {
+        $newMoon = (new JdToLunarNewMoon(
+            jd: $this->lunar->j,
+            offset: $this->offset
+        ))->getOutput();
+
+        $navigator = new NewMoonIterator($newMoon, $this->offset);
+        $navigator->next();
+
+        $nextNm = $navigator->getOutput();
+
+        return $nextNm->jd - $newMoon->jd;
+    }
+
+    /**
+     * 'P' formated
+     *
+     * @return string
+     */
+    protected function P(): string
+    {
+        $h = $this->offset / 3600;
+        $d = abs($h - floor($h));
+
+        $h = ($h >= 0)
+            ? str_pad($h, 3, '+0', STR_PAD_LEFT)
+            : str_pad(abs($h), 3, '-0', STR_PAD_LEFT);
+
+        $p = $h . ':' . str_pad($d, 2, '0', STR_PAD_LEFT);
+
+        return $p;
+    }
+
+    /**
+     * 'c' formated
+     *
+     * @return string
+     */
+    protected function c(): string
+    {
+        $str = implode('-', [
+            $this->getEquivalent('Y'),
+            $this->getEquivalent('m'),
+            $this->getEquivalent('d')
+        ]);
+
+        $str .= 'T';
+        $str .= implode(':', [
+            $this->getEquivalent('H'),
+            $this->getEquivalent('i'),
+            $this->getEquivalent('s')
+        ]);
+
+        $str .= $this->getEquivalent('P');
+        $str .= $this->getEquivalent('k');
+
+        return rtrim($str);
+    }
+
+    /**
+     * Get equivalent value of format character
+     *
+     * @param string $char
+     * @return string
+     */
+    protected function getEquivalent(string $char): string
+    {
+        $equivalent = match ($char) {
+            // Day of the month without leading zeros
+            'j' => $this->lunar->d,
+
+            // Day of the month, 2 digits with leading zeros
+            'd' => str_pad($this->lunar->d, 2, '0', STR_PAD_LEFT),
+
+            // Numeric representation of a month with leap sign, without leading zeros
+            'l' => $this->lunar->leap ? $this->lunar->m . '+' : $this->lunar->m,
+
+            // Numeric representation of a month with leap sign, with leading zeros
+            'L' => str_pad($this->getEquivalent('l'), 2, '0', STR_PAD_LEFT),
+
+            // Numeric representation of a month, without leading zeros and leap sign
+            'n' => $this->lunar->m,
+
+            // Numeric representation of a month, with leading zeros and leap sign
+            'm' => str_pad($this->lunar->m, 2, '0', STR_PAD_LEFT),
+
+            // A full numeric representation of a year, at least 4 digits
+            'Y' => str_pad($this->lunar->y, 4, '0', STR_PAD_LEFT),
+
+            // Total days of current month
+            't' => $this->t(),
+
+            // 24-hour format of an hour without leading zeros
+            'G' => $this->lunar->h,
+
+            // 12-hour format of an hour without leading zeros
+            'g' => $this->lunar->h > 12 ? $this->lunar->h % 12 : $this->lunar->h,
+
+            // 24-hour format of an hour with leading zeros
+            'H' => str_pad($this->lunar->h, 2, '0', STR_PAD_LEFT),
+
+            // 12-hour format of an hour with leading zeros
+            'h' => str_pad($this->getEquivalent('g'), 2, '0', STR_PAD_LEFT),
+
+            // Minutes with leading zeros
+            'i' => str_pad($this->lunar->i, 2, '0', STR_PAD_LEFT),
+
+            // Seconds with leading zeros
+            's' => str_pad($this->lunar->s, 2, '0', STR_PAD_LEFT),
+
+            // Lowercase Ante meridiem and Post meridiem
+            'a' => $this->lunar->h < 12 ? 'am' : 'pm',
+
+            // Uppercase Ante meridiem and Post meridiem
+            'A' => $this->lunar->h < 12 ? 'AM' : 'PM',
+
+            // Difference to Greenwich time (GMT) with colon between hours and minutes
+            'P' => $this->P(),
+
+            // Difference to Greenwich time (GMT) without colon between hours and minutes
+            'O' => str_replace(':', '', $this->P()),
+
+            // Seconds since the Unix Epoch (January 1 1970 00:00:00 GMT)
+            'U' => floor(($this->lunar->j - 2440587.5) * 86400),
+
+            // Timezone offset in seconds. The offset for timezones west of UTC is always negative, 
+            // and for those east of UTC is always positive.
+            'Z' => $this->offset,
+
+            // Leap month sign (+)
+            'k' => $this->lunar->leap ? '(+)' : '',
+
+            // Leap month sign [-]
+            'K' => $this->lunar->leap ? '[+]' : '',
+
+            // Timezone identifier
+            'e' => $this->timezone->getName(),
+
+            // A lunar ISO 8601 date version 
+            'c' => $this->c(),
+
+            default => $char
+        };
+
+        return (string)$equivalent;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function format(string $formatter): string
+    {
+        $result = $formatter;
+
+        foreach (str_split($formatter) as $char) {
+            $result = str_replace($char, $this->getEquivalent($char), $result);
+        }
+
+        return trim($result);
+    }
+}
---
+++ tests/Formatters/DefaultLunarDateTimeFormatterTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace LucNham\LunarCalendar\Formatters\Tests;
+
+use DateTimeZone;
+use LucNham\LunarCalendar\Converters\JdToGregorian;
+use LucNham\LunarCalendar\Converters\JdToLunarNewMoon;
+use LucNham\LunarCalendar\Converters\JdToMidnightJd;
+use LucNham\LunarCalendar\Converters\JdToTime;
+use LucNham\LunarCalendar\Converters\NewMoonIterator;
+use LucNham\LunarCalendar\Formatters\LunarDateTimeDefaultFormatter;
+use LucNham\LunarCalendar\Terms\LunarDateTimeGuaranteed;
+use LucNham\LunarCalendar\Terms\NewMoonPhase;
+use LucNham\LunarCalendar\Terms\TimeInterval;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(LunarDateTimeDefaultFormatter::class)]
+#[CoversClass(LunarDateTimeGuaranteed::class)]
+#[CoversClass(JdToGregorian::class)]
+#[CoversClass(JdToLunarNewMoon::class)]
+#[CoversClass(NewMoonIterator::class)]
+#[CoversClass(NewMoonPhase::class)]
+#[CoversClass(TimeInterval::class)]
+#[CoversClass(JdToTime::class)]
+#[CoversClass(JdToMidnightJd::class)]
+class DefaultLunarDateTimeFormatterTest extends TestCase
+{
+    public function testFormating()
+    {
+        $interval = new LunarDateTimeGuaranteed(
+            d: 1,
+            m: 2,
+            y: 2024,
+            h: 3,
+            i: 4,
+            s: 5,
+            l: 0,
+            leap: false,
+            j: 2460379.919502,
+        );
+
+        $timezone = new DateTimeZone('Asia/Ho_Chi_Minh');
+        $offset = 25200;
+        $formatter = new LunarDateTimeDefaultFormatter($interval, $timezone, $offset);
+
+        $this->assertEquals('1', $formatter->format('j'));
+        $this->assertEquals('01', $formatter->format('d'));
+
+        $this->assertEquals('2', $formatter->format('n'));
+        $this->assertEquals('02', $formatter->format('m'));
+
+        $this->assertEquals('2', $formatter->format('l'));
+        $this->assertEquals('02', $formatter->format('L'));
+
+        $this->assertEquals('2024', $formatter->format('Y'));
+
+        $this->assertEquals('30', $formatter->format('t'));
+
+        $this->assertEquals('3', $formatter->format('g'));
+        $this->assertEquals('3', $formatter->format('G'));
+
+        $this->assertEquals('03', $formatter->format('h'));
+        $this->assertEquals('03', $formatter->format('H'));
+
+        $this->assertEquals('04', $formatter->format('i'));
+
+        $this->assertEquals('05', $formatter->format('s'));
+
+        $this->assertEquals('am', $formatter->format('a'));
+        $this->assertEquals('AM', $formatter->format('A'));
+
+        $this->assertEquals('+07:00', $formatter->format('P'));
+        $this->assertEquals('+0700', $formatter->format('O'));
+
+        $this->assertEquals('1710065044', $formatter->format('U'));
+        $this->assertEquals('25200', $formatter->format('Z'));
+
+        $this->assertEquals('', $formatter->format('K'));
+        $this->assertEquals('', $formatter->format('k'));
+
+        $this->assertEquals('Asia/Ho_Chi_Minh', $formatter->format('e'));
+
+        $this->assertEquals('2024-02-01T03:04:05+07:00', $formatter->format('c'));
+
+        $this->assertEquals('2024-02-01 03:04:05 +07:00', $formatter->format('Y-m-d H:i:s P k'));
+    }
+
+    public function testLeapSign()
+    {
+        $interval = new LunarDateTimeGuaranteed(
+            d: 10,
+            m: 11,
+            y: 2033,
+            h: 10,
+            i: 20,
+            s: 30,
+            l: 11,
+            leap: true,
+            j: 2463963.222569,
+        );
+
+        $timezone = new DateTimeZone('+0700');
+        $offset = 25200;
+        $formatter = new LunarDateTimeDefaultFormatter($interval, $timezone, $offset);
+
+        $this->assertEquals('(+)', $formatter->format('k'));
+        $this->assertEquals('[+]', $formatter->format('K'));
+
+        $this->assertEquals('11+', $formatter->format('l'));
+        $this->assertEquals('11+', $formatter->format('L'));
+
+        $this->assertEquals('10/11+/2033', $formatter->format('d/L/Y'));
+        $this->assertEquals('2033-11-10T10:20:30+07:00(+)', $formatter->format('c'));
+
+        $this->assertEquals('2033-11-10 10:20 (+)', $formatter->format('Y-m-d H:i k'));
+        $this->assertEquals('2033-11-10 10:20 [+]', $formatter->format('Y-m-d H:i K'));
+    }
+
+    public function testWestLocations()
+    {
+        $interval = new LunarDateTimeGuaranteed(
+            d: 1,
+            m: 2,
+            y: 2024,
+            h: 3,
+            i: 4,
+            s: 5,
+            l: 0,
+            leap: false,
+            j: 2460379.919502,
+        );
+
+        $timezone = new DateTimeZone('-04:00');
+        $offset = -4 * 3600;
+        $formatter = new LunarDateTimeDefaultFormatter($interval, $timezone, $offset);
+
+        $this->assertEquals('-04:00', $formatter->format('P'));
+        $this->assertEquals('-0400', $formatter->format('O'));
+    }
+}
