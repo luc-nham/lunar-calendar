0047bfd07d0b4e1c95ebb3669242404f22c12a24
vantran445 <admin@lucnham.com>
2024-10-20 11:18:04 +0700

    Add Jd to unix timestamp converter

---
+++ src/Converters/JdToUnix.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LucNham\LunarCalendar\Converters;
+
+use LucNham\LunarCalendar\Converters\Traits\JdInputSetable;
+
+/**
+ * Converts Julian day number to  Unix timestamp
+ */
+class JdToUnix extends Converter
+{
+    use JdInputSetable;
+
+    /**
+     * Create new converter
+     *
+     * @param float $jd Julian day number, default 2440587.5 corresponds to 1970-01-01 00:00 +00:00
+     */
+    public function __construct(private float $jd = 2440587.5) {}
+
+    /**
+     * Returns Unix timestamp corresponding to Julian day number input
+     *
+     * @return int
+     */
+    public function getOutput(): int
+    {
+        return ($this->jd - 2440587.5) * 86400;
+    }
+}
