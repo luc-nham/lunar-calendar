444f30b0d50bc4fe10c3aa827ac98aef50afcd61
vantran445 <admin@lucnham.com>
2024-10-24 21:59:30 +0700

    Add new inheritance to get guaranteed Lunar date time

--- src/Contracts/LunarDateTime.php
+++ src/Contracts/LunarDateTime.php
@@ -5,7 +5,7 @@ namespace LucNham\LunarCalendar\Contracts;
 /**
  * Representation of Lunar calendar date and time
  */
-interface LunarDateTime extends ZoneAccessible, LunarDateTimeFormattable
+interface LunarDateTime extends ZoneAccessible, LunarDateTimeFormattable, LunarGuaranteedAccessible
 {
     /**
      * Gets the Unix timestamp
