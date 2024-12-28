223bb0b4c4b033fe3f9950eeab7ae0a907db4bc5
luc-nham <admin@lucnham.com>
2024-12-28 14:34:17 +0700

    Add new toString method to make PHP date string quickly

--- src/Terms/DateTimeInterval.php
+++ src/Terms/DateTimeInterval.php
@@ -16,4 +16,26 @@ readonly class DateTimeInterval
         public int $i = 0,
         public int $s = 0,
     ) {}
+
+    /**
+     * Convert properties to valid PHP date string. Supported characters include: Y, m, d, H, i, s
+     *
+     * @param string $format
+     * @return string
+     */
+    public function toString(string $format = 'Y-m-d H:i:s'): string
+    {
+        $pad = function (int $v, int $q = 2): string {
+            return str_pad($v, $q, '0', STR_PAD_LEFT);
+        };
+
+        return strtr($format, [
+            'Y' => $pad($this->y, 4),
+            'm' => $pad($this->m),
+            'd' => $pad($this->d),
+            'H' => $pad($this->h),
+            'i' => $pad($this->i),
+            's' => $pad($this->s),
+        ]);
+    }
 }
